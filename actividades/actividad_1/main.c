ebe5abb517ca99292b70638b21c55c42e6688715
beni <142276080+Ramirez-Hilz-Benicio@users.noreply.github.com>
2024-07-12 10:49:54 -0300

    main.c

--- actividades/actividad_1/main.c
+++ actividades/actividad_1/main.c
@@ -10,7 +10,7 @@
  * @brief Programa principal
  */ 
 int main() {
-    // Variable para almacenar el resultado del ADC
+    // Variable para almacenar el resultado del ADC/
     uint16_t adc_value = 0;
     // Variable para guardar el valor de temperatura
     float temperatura = 0.0;
@@ -18,8 +18,10 @@ int main() {
     const uint16_t beta = 3950;
     // Habilito USB
     stdio_init_all();
+
+    
     // Configuro el I2C0 a 100 KHz de clock
-    i2c_init(i2c0, 100 * 1000);
+    //i2c_init(i2c0, 100 * 1000);
     // Elijo GPIO4 como linea de SDA
     gpio_set_function(4, GPIO_FUNC_I2C);
     // Elijo GPIO5 como linea de SCL
@@ -31,24 +33,26 @@ int main() {
     // Inicializacion del LCD
     lcd_init();
     // Inicializo ADC
-
+    adc_gpio_init(26);
+    adc_init();
+    adc_select_input(0);
     // Inicializo GPIO26 como entrada analogica
-
+    
     // Selecciono canal analogico
 
     while(true) {
         // Leer NTC
-        
+        adc_value = adc_read();
         // Calculo temperatura
-        
+        float temperatura= 1 / (log(1 / (4095. / adc_value - 1)) / beta + 1.0 / 298.15) - 273.15;
         // Limpio LCD
-        lcd_clear();
+    //    lcd_clear();
         // Variable para el string
         char str[16];
         // Creo string
-        
+        printf("El valor de la temperatura es:%2f\n", temperatura);
         // Imprimo string en segunda fila
-        lcd_string(str);
+    //    lcd_string(str);
         // Espero 500 ms
         sleep_ms(500);
     }
